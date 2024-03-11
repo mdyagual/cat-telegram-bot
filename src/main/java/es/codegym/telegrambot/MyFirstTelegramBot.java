93d7672a1c737d28986efb976d9215f0d972c37b
Mishell Yagual <mcym_1995@hotmail.com>
2024-03-11 00:03:20 -0500

    Configure & send hello to the bot

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
-    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
-    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+    public static final String NAME = "cat_marathon_bot";
+    public static final String TOKEN = "7019493472:AAEoyS2MaIc_pk976R9GpTuI779yMszduZ4";
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -17,6 +17,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
+        sendTextMessageAsync("Holi, este es el primer bot que hago :3");
     }
 
     public static void main(String[] args) throws TelegramApiException {
