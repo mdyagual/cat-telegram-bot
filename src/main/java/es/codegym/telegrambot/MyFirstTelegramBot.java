d7cb0bae0b4a9bdbe216a3e8eb6d443ff758502a
Mishell Yagual <mcym_1995@hotmail.com>
2024-03-13 22:59:21 -0500

    Day 4 - Configure more stuff to the bot: Adding photos

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -28,6 +28,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     public void startGame(String text){
         if(text.toLowerCase().equals("/start")){
             setUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT, Map.of("Hackear nevera","step_1_btn"));
         }else if(text.toLowerCase().contains("descansa")){
             sendTextMessageAsync("Gracias Karen~");
@@ -44,6 +45,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         switch (step){
             case "step_1_btn":
                 setUserGlory(20);
+                sendPhotoMessageAsync("step_2_pic");
                 sendTextMessageAsync(STEP_2_TEXT, Map.of("¡Tomar una salchicha! +20 ptos","step_2_btn",
                         "¡Tomar un pescadito! +20 ptos","step_2_btn" ,
                         "¡Tirar una lata de pepinillos! +20 ptos","step_2_btn"));
@@ -51,11 +53,13 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
             case "step_2_btn":
                 setUserGlory(20);
+                sendPhotoMessageAsync("step_3_pic");
                 sendTextMessageAsync(STEP_3_TEXT, Map.of("¡Hackear/destruir al robot aspirador!","step_3_btn"));
                 break;
 
             case "step_3_btn":
                 setUserGlory(70);
+                sendPhotoMessageAsync("step_4_pic");
                 sendTextMessageAsync(STEP_4_TEXT, Map.of("Enviar al robot aspirador por comida. +30 ptos","step_4_btn",
                         "Pasear en el robot aspirador +30 ptos","step_4_btn" ,
                         "¡Huir del robot aspirador! +20 ptos","step_4_btn"));
@@ -63,24 +67,32 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
             case "step_4_btn":
                 //setUserGlory(30);
+                sendPhotoMessageAsync("step_5_pic");
                 sendTextMessageAsync(STEP_5_TEXT, Map.of("Colocar y encender la GoPro","step_5_btn"));
                 break;
 
             case "step_5_btn":
                 setUserGlory(40);
+                sendPhotoMessageAsync("step_6_pic");
                 sendTextMessageAsync(STEP_6_TEXT, Map.of("¡Hora loca!","step_6_btn",
                         "Molestar al perro","step_6_btn",
                         "Cazar insectos","step_6_btn"));
                 break;
             case "step_6_btn":
                 //setUserGlory(40);
+                sendPhotoMessageAsync("step_7_pic");
                 sendTextMessageAsync(STEP_7_TEXT, Map.of("Hackear computadora de la Karen","step_7_btn"));
                 break;
             case "step_7_btn":
                 setUserGlory(50);
+                sendPhotoMessageAsync("step_8_pic");
+                sendTextMessageAsync(STEP_8_TEXT, Map.of("Salir al patio","step_8_btn"));
+                break;
+            case "step_8_btn":
+                setUserGlory(50);
+                sendPhotoMessageAsync("final_pic");
                 sendTextMessageAsync(FINAL_TEXT);
                 break;
-
         }
 
 
